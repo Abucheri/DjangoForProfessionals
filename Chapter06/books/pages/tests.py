043a7348ae592d266d98891a7c5905eb197eab4e
Ravi Indorey <ravi.indorey90@gmail.com>
2020-06-30 15:43:48 +0530

    Test: Static Pages

--- Chapter06/books/pages/tests.py
+++ Chapter06/books/pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 class HomePageTest(SimpleTestCase):
@@ -22,3 +22,23 @@ class HomePageTest(SimpleTestCase):
         resolve_obj = resolve('/')
         self.assertEqual(resolve_obj.func.__name__,
                          HomePageView.as_view().__name__)
+
+
+class AboutPageTest(SimpleTestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse('about'))
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
+
+    def test_aboutpage_url_resolve_aboutpageview(self):
+        resolve_obj = resolve(reverse('about'))
+        self.assertEqual(resolve_obj.func.__name__,
+                         AboutPageView.as_view().__name__)
