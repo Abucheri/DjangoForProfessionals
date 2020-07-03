d58ccea135c7fa511fe7fb4e4b318dab3453f633
Ravi Indorey <ravi.indorey90@gmail.com>
2020-07-04 00:25:03 +0530

    Managing Secrets

--- Chapter08/books/bookstore_project/settings.py
+++ Chapter08/books/bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(fm!@75&m#s)j7^4#qtoazzj@f$3n2=bkkg6-26j#p%xbmy(qq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('ENV') == 'development' else False
 
 ALLOWED_HOSTS = []
 
