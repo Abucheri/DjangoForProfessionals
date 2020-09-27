fb7dacb3ea5cff037ccbca56c70bc48948337d96
Ravi Indorey <ravi.indorey90@gmail.com>
2020-09-28 01:53:17 +0530

    Only authenticated users can see books

--- Chapter13/books/books/views.py
+++ Chapter13/books/books/views.py
@@ -1,15 +1,18 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 
 from .models import Book
 
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
+    login_url = 'account_login'
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, DetailView):
+    login_url = 'account_login'
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
