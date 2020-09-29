f4d2ee802f7eeba04bc5e2409f4c485c33fd0b2b
Ravi Indorey <ravi.indorey90@gmail.com>
2020-09-29 23:35:19 +0530

    Add Stripe: Complete
    Followed the book but implementation could be way better.
    1. Current implementation won't work for all countries.
    2. Unsuccessful purchase would still provide customer to all books.
    3. Probably Braintree is better, idk.

--- Chapter14/books/orders/views.py
+++ Chapter14/books/orders/views.py
@@ -1,6 +1,7 @@
 import stripe
-from django.views.generic import TemplateView
 from django.conf import settings
+from django.views.generic import TemplateView
+from django.contrib.auth.models import Permission
 from django.shortcuts import render
 
 stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
@@ -16,6 +17,10 @@ class OrdersPageView(TemplateView):
 
 
 def charge(request):
+    # Not the best way
+    permission = Permission.objects.get(codename='special_status')
+    user = request.user
+    user.user_permissions.add(permission)
     if request.method == 'POST':
         # Integration for India work differently
         stripe.Charge.create(
