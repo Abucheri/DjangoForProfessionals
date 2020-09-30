ce679b1039e84ca737f7038f2816abd7a3741e3e
Ravi Indorey <ravi.indorey90@gmail.com>
2020-09-30 13:25:58 +0530

    QuerySet: Q objects

--- Chapter15/books/books/views.py
+++ Chapter15/books/books/views.py
@@ -1,8 +1,9 @@
+from django.db.models import Q
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin,
 )
-from django.views.generic import ListView, DetailView
 
 from .models import Book
 
@@ -20,3 +21,15 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
+
+
+class SearchResultsView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    # queryset = Book.objects.filter(title__icontains='beginners')
+
+    def get_queryset(self):
+        return Book.objects.filter(
+            Q(title__icontains='beginners') | Q(title__icontains='api')
+        )
