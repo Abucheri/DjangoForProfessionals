2a8dec64e15db41fa987c41eba09cf714cc0bcb3
Ravi Indorey <ravi.indorey90@gmail.com>
2020-09-30 22:48:58 +0530

    Different setting for production

--- Chapter17/books/bookstore_project/settings.py
+++ Chapter17/books/bookstore_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('ENV') == 'development' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
