277605bec0fc4ac0f33dcda2be04e996aa0c1a77
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-01 00:16:37 +0530

    Setup WhiteNoise

--- Chapter18/books/bookstore_project/settings.py
+++ Chapter18/books/bookstore_project/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',    # whitenoise (3rd party)
     'django.contrib.staticfiles',
-    'django.contrib.sites',         # added
+    'django.contrib.sites',             # added
 
     # 3rd Party
     'crispy_forms',
@@ -59,6 +60,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
